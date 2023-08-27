9f102ebffb2b8922669f9f0abd0432bc132e8715
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-08-27 08:21:49 +0600

    Put route is running

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -1,86 +1,186 @@
 const lib = require('../../lib/data');
 const { hash } = require('../../helpers/utilities');
+const { perseJSON } = require('../../helpers/utilities');
 
 const handle = {};
 
 handle.userHandler = (requestProperties, callback) => {
-  const acceptedMethods = ['get', 'post', 'put', 'delete'];
+    const acceptedMethods = ['get', 'post', 'put', 'delete'];
 
-  if (acceptedMethods.indexOf(requestProperties.method) > -1) {
-    handle._users[requestProperties.method](requestProperties, callback);
-  } else {
-    callback(405, {
-      message: 'Request failed, try get, post, put or delete',
-    });
-  }
+    if (acceptedMethods.indexOf(requestProperties.method) > -1) {
+        handle._users[requestProperties.method](requestProperties, callback);
+    } else {
+        callback(405, {
+            message: 'Request failed, try  post, get, put or delete',
+        });
+    }
 };
 
 handle._users = {};
 
-handle._users.get = (requestProperties, callback) => {
-  callback(405, {
-    message: 'Get page loaded',
-  });
-};
 handle._users.post = (requestProperties, callback) => {
-  const firstName =
-    typeof requestProperties.body.firstName === 'string' &&
-    requestProperties.body.firstName.trim().length > 0
-      ? requestProperties.body.firstName
-      : null;
-  const lastName =
-    typeof requestProperties.body.lastName === 'string' &&
-    requestProperties.body.lastName.trim().length > 0
-      ? requestProperties.body.lastName
-      : null;
-  const phone =
-    typeof requestProperties.body.phone === 'string' &&
-    requestProperties.body.phone.trim().length === 11
-      ? requestProperties.body.phone
-      : null;
-  const password =
-    typeof requestProperties.body.password === 'string' &&
-    requestProperties.body.password.trim().length > 0
-      ? requestProperties.body.password
-      : null;
-  const tosAgree =
-    typeof requestProperties.body.tosAgree === 'boolean' && requestProperties.body.tosAgree
-      ? requestProperties.body.tosAgree
-      : null;
-  if (firstName && lastName && phone && password && tosAgree) {
-    lib.read('users', phone, (err) => {
-      if (err) {
-        const userObject = {
-          firstName,
-          lastName,
-          phone,
-          password: hash(password),
-          tosAgree,
-        };
-        lib.create('users', phone, userObject, (err2) => {
-          console.log(err2);
-          if (!err2) {
-            callback(200, {
-              Success: 'User created',
-            });
-          } else {
-            callback(500, {
-              Error: 'Could not create user',
-            });
-          }
+    const firstName =
+        typeof requestProperties.body.firstName === 'string' &&
+        requestProperties.body.firstName.trim().length > 0
+            ? requestProperties.body.firstName
+            : null;
+
+    const lastName =
+        typeof requestProperties.body.lastName === 'string' &&
+        requestProperties.body.lastName.trim().length > 0
+            ? requestProperties.body.lastName
+            : null;
+
+    const phone =
+        typeof requestProperties.body.phone === 'string' &&
+        requestProperties.body.phone.trim().length === 11
+            ? requestProperties.body.phone
+            : null;
+
+    const password =
+        typeof requestProperties.body.password === 'string' &&
+        requestProperties.body.password.trim().length > 0
+            ? requestProperties.body.password
+            : null;
+
+    const tosAgree =
+        typeof requestProperties.body.tosAgree === 'boolean' && requestProperties.body.tosAgree
+            ? requestProperties.body.tosAgree
+            : null;
+
+    if (firstName && lastName && phone && password && tosAgree) {
+        lib.read('users', phone, (err) => {
+            if (err) {
+                const userObject = {
+                    firstName,
+                    lastName,
+                    phone,
+                    password: hash(password),
+                    tosAgree,
+                };
+                lib.create('users', phone, userObject, (err2) => {
+                    console.log(err2);
+                    if (!err2) {
+                        callback(200, {
+                            Success: 'User created',
+                        });
+                    } else {
+                        callback(500, {
+                            Error: 'Could not create user',
+                        });
+                    }
+                });
+            } else {
+                callback(500, {
+                    Error: 'There was a problem, may user already exist',
+                });
+            }
         });
-      } else {
-        callback(500, {
-          Error: 'There was a problem, may user already exist',
+    } else {
+        callback(400, {
+            Error: 'There is a probelm with your request',
         });
-      }
-    });
-  } else {
-    callback(400, {
-      Error: 'There is a probelm with your request',
-    });
-  }
+    }
 };
-handle._users.put = (requestProperties, callback) => {};
+
+handle._users.get = (requestProperties, callback) => {
+    const phone =
+        typeof requestProperties.queryStringObject.phone === 'string' &&
+        requestProperties.queryStringObject.phone.trim().length === 11
+            ? requestProperties.queryStringObject.phone
+            : false;
+    if (phone) {
+        lib.read('users', phone, (err, useData) => {
+            const user = { ...perseJSON(useData) };
+            if (!err && user) {
+                delete user.password;
+                callback(200, user);
+            } else {
+                callback(404, {
+                    error: 'User not found! Create new user',
+                });
+            }
+        });
+    } else {
+        callback(404, {
+            error: 'Invalid rquest',
+        });
+    }
+};
+
+handle._users.put = (requestProperties, callback) => {
+    const firstName =
+        typeof requestProperties.body.firstName === 'string' &&
+        requestProperties.body.firstName.trim().length > 0
+            ? requestProperties.body.firstName
+            : null;
+
+    const lastName =
+        typeof requestProperties.body.lastName === 'string' &&
+        requestProperties.body.lastName.trim().length > 0
+            ? requestProperties.body.lastName
+            : null;
+
+    const phone =
+        typeof requestProperties.body.phone === 'string' &&
+        requestProperties.body.phone.trim().length === 11
+            ? requestProperties.body.phone
+            : null;
+
+    const password =
+        typeof requestProperties.body.password === 'string' &&
+        requestProperties.body.password.trim().length > 0
+            ? requestProperties.body.password
+            : null;
+
+    const tosAgree =
+        typeof requestProperties.body.tosAgree === 'boolean' && requestProperties.body.tosAgree
+            ? requestProperties.body.tosAgree
+            : null;
+
+    if (phone) {
+        if (firstName || lastName || password) {
+            lib.read('users', phone, (err, uData) => {
+                const userData = { ...perseJSON(uData) };
+                if (!err && userData) {
+                    if (firstName) {
+                        userData.firstName = firstName;
+                    }
+                    if (lastName) {
+                        userData.lastName = lastName;
+                    }
+                    if (password) {
+                        userData.password = hash(password);
+                    }
+
+                    lib.update('user', phone, userData, (err) => {
+                        if (!err) {
+                            callback(200, {
+                                Error: 'Data updated successfully!',
+                            });
+                        } else {
+                            console.log(err);
+                            callback(400, {
+                                Error: 'There was a error updating data!',
+                            });
+                        }
+                    });
+                } else {
+                    callback(400, {
+                        Error: 'Invalid phone number, please try again!',
+                    });
+                }
+            });
+        } else {
+            callback(400, {
+                Error: 'Please provide update data',
+            });
+        }
+    } else {
+        callback(400, {
+            Error: 'Invalid phone number, please try again!',
+        });
+    }
+};
 handle._users.delete = (requestProperties, callback) => {};
 module.exports = handle;
--- helpers/utilities.js
+++ helpers/utilities.js
@@ -3,24 +3,26 @@ const envExport = require('./env');
 const utilites = {};
 
 utilites.perseJSON = (jsonString) => {
-  let output = {};
+    let output = {};
 
-  try {
-    output = JSON.parse(jsonString);
-  } catch (err) {
-    output = { err };
-  }
+    try {
+        output = JSON.parse(jsonString);
+    } catch (err) {
+        output = { err };
+    }
 
-  return output;
+    return output;
 };
 
+console.log(this.perseJSON);
+
 utilites.hash = (string) => {
-  if (typeof string === 'string' && string.length > 0) {
-    const hash = crypto.createHmac('sha256', envExport.secretKey).update(string).digest('hex');
-    return hash;
-  } else {
-    return null;
-  }
+    if (typeof string === 'string' && string.length > 0) {
+        const hash = crypto.createHmac('sha256', envExport.secretKey).update(string).digest('hex');
+        return hash;
+    } else {
+        return null;
+    }
 };
 
 module.exports = utilites;
