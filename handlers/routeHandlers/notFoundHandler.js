93384da9013c960beb918b60e756a7ef1b7bb8db
Shumon Khan <132203268+codewithshumon@users.noreply.github.com>
2023-09-13 12:55:25 +0600

    Delete handlers/routeHandlers directory

--- handlers/routeHandlers/notFoundHandler.js
+++
@@ -1,9 +0,0 @@
-const handle = {};
-
-handle.notFoundHandler = (requestProperties, callback) => {
-  callback(404, {
-    message: 'Page not found',
-  });
-};
-
-module.exports = handle;
--- handlers/routeHandlers/sampleHandler.js
+++
@@ -1,9 +0,0 @@
-const handle = {};
-
-handle.sampleHandler = (requestProperties, callback) => {
-  callback(200, {
-    message: 'This is a sample page',
-  });
-};
-
-module.exports = handle;
